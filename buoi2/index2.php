5d94ada1f0de6bdb780e9581a515fd640fa4556e
duong53 <167328510+duong53@users.noreply.github.com>
2025-11-24 23:06:54 +0700

    Create index2.php

---
+++ buoi2/index2.php
@@ -0,0 +1,22 @@
+<?php
+    //cookie
+    #lưu ở phía người dùng
+    #dùng cho những thông tin ít quan trọng
+    $cookieName = "user";
+    $cookieValue = "HoangDuong";
+    //86400 = 30ngay
+    //setcookie($cookieName, $cookieValue, time() + (86400), "/" )
+
+     if(isset($_COOKIE[$cookieName]) ){
+        echo "cookie da ton tai";
+     }else{
+        echo "cookie chua ton tai";
+     }
+
+    //session 
+    #Thông tin đăng nhập,giỏ hàng....
+    session_start();
+    $_SESSION['name'] = "Hoang Duong";
+    echo "<br>".$_SESSION['name'];
+    
+?>

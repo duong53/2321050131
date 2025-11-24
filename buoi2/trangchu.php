3a095f4aca4b2103d5a53db178286b409726c2ea
duong53 <167328510+duong53@users.noreply.github.com>
2025-11-24 23:06:52 +0700

    Create trangchu.php

---
+++ buoi2/trangchu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("Location: index.php");
+    }
+    
+    echo "Xin chào "
+    ?>
+    <h1>Trang chủ</h1>
+</body>
+</html>

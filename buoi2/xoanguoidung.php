7e571bbc35e94ba751ce514ab6ded112a86beee8
duong53 <167328510+duong53@users.noreply.github.com>
2025-11-28 11:37:16 +0700

    Create xoanguoidung.php

---
+++ buoi2/xoanguoidung.php
@@ -0,0 +1,8 @@
+<?php
+include("connect.php");
+$id = $_GET['id'];
+$sql = "DELETE FROM `nguoi_dung` WHERE id = $id";
+mysqli_query($conn,$sql);
+header("Location: index.php?page_layout=nguoidung");
+
+?>
